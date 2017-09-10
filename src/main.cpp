f5bd5ed57506a2ea822b34f9c989a6044c7b92fc
Kevin Luedemann <kevin27luedemann@googlemail.com>
2017-09-10 15:27:53 +0200

    more smoothe Programming

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //this example runs with 2 shift registers
 
-#define F_CPU 8000000
+#define F_CPU 3686400
 #define BAUD 9600
 
 #include <stdio.h>
@@ -28,8 +28,9 @@ nokia_5110 nok(&DC,&CS,&RST,&spi);
 Input taster(&DDRD,&PORTD,&PIND,2,true);
 
 #define DISP_UPDATE 0
-#define TURNED 1
-#define INCREMENT 2
+#define INCREMENT 1
+#define DECREMENT 2
+#define BACKLIGHT 3
 uint8_t flag_reg;
 
 uint8_t sekunden;
@@ -44,36 +45,43 @@ ISR(USART_RX_vect){
 
 ISR(INT1_vect){
     if(RTDT.ison()){
-        if(sekunden<100){sekunden++;}
+        //if(sekunden<100){sekunden++;}
+        flag_reg |= (1<<INCREMENT);
     }
     else{
-        if(sekunden<=100 && sekunden>0){sekunden--;}
+        //if(sekunden<=100 && sekunden>0){sekunden--;}
+        flag_reg |= (1<<DECREMENT);
     }
     flag_reg |= (1<<DISP_UPDATE);
 }
 
+ISR(INT0_vect){
+    if(flag_reg&(1<<BACKLIGHT)){flag_reg&=~(1<<BACKLIGHT);}
+    else{flag_reg|=(1<<BACKLIGHT);}
+    flag_reg |= (1<<DISP_UPDATE);
+}
+
 void update_disp();
-void nachti(uint8_t mode);
+void nachti();
+void blpwm(uint8_t on);
 
 int main(void) {
 
     ACSR = (1<<ACD);
     //uart_init();
     flag_reg = 0;
-    //BL.on();
 
     //seconds timer
     TIMSK1 = (1 << OCIE1A);
-    OCR1A  = 31250;
+    OCR1A  = 14400;
     TCCR1B = (1 << WGM12) | (1 << CS12); //CTC Mode, Presc 256
 
     //interupt detection
     EICRA |= (1<<ISC11);// | (1<<ISC10);
-    EIMSK |= (1<<INT1);
+    EICRA |= (1<<ISC01) | (1<<ISC00);
+    EIMSK |= (1<<INT1) | (1<<INT0);
 
     //fast PWM for BL
-    TCCR0A = (1<<COM0A1) | (1<<WGM01) | (1<<WGM00);
-    TCCR0B = (1<<CS02);
     OCR0A  = 128;
     sekunden=50;
     sei();
@@ -82,36 +90,49 @@ int main(void) {
 
 	while(true) 
     {
-        //if(taster.ison()){LED.on();sekunden++;}
         if(RTSW.ison()){LED.on();}
         else{LED.off();}
 
+        if((flag_reg&(1<<BACKLIGHT))&&!(TCCR0B&(1<<CS02))){blpwm(1);}
+        else if(!(flag_reg&(1<<BACKLIGHT))&&(TCCR0B&(1<<CS02))){blpwm(0);}
+
+        if(flag_reg&(1<<INCREMENT)){if(sekunden<100){sekunden++;}flag_reg&=~(1<<INCREMENT);}
+        else if(flag_reg&(1<<DECREMENT)){if(sekunden<=100&&sekunden>0){sekunden--;}flag_reg&=~(1<<DECREMENT);}
+
         if((flag_reg&(1<<DISP_UPDATE))){update_disp();flag_reg&=~(1<<DISP_UPDATE);}
 
-        nachti(0);
+        nachti();
 	}
 
 }
 
-void nachti(uint8_t mode){
-    switch (mode)
-    {
-        case 0: 
-            set_sleep_mode(SLEEP_MODE_IDLE);
-            break;
-        case 1:
-            set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-            break;
-        case 2:
-            set_sleep_mode(SLEEP_MODE_STANDBY);
-            break;
+void blpwm(uint8_t on){
+    if(on){
+        TCCR0A = (1<<COM0A1) | (1<<WGM01) | (1<<WGM00);
+        TCCR0B = (1<<CS02);
+    }
+    else{
+        TCCR0B = 0;
+        TCCR0A = 0;
+        BL.off();
+    }
+}
+
+void nachti(){
+    if(flag_reg&(1<<BACKLIGHT)){
+        set_sleep_mode(SLEEP_MODE_IDLE);
+    }
+    else{
+        set_sleep_mode(SLEEP_MODE_PWR_SAVE);
     }
     sleep_mode();
 }
 
 void update_disp(){
-    if(sekunden>100){sekunden=100;}
-    OCR0A = (uint8_t)((float)sekunden*2.55);
+    if(flag_reg&(1<<BACKLIGHT)){
+        if(sekunden>100){sekunden=100;}
+        OCR0A = (uint8_t)((float)sekunden*2.55);
+    }
     nok.clearDisplay();
     //nok.analog(10,25,sekunden,true);
     if(sekunden<0){
